258ea35f57301b45b74171a5fc110a1511118c29
davilucas <davilucarios47@gmail.com>
2024-05-21 22:15:51 -0300

    Update produto.js

--- codigo-fonte/tela-produto/produto.js
+++ codigo-fonte/tela-produto/produto.js
@@ -1,8 +1,16 @@
 let botao = document.querySelector('#botao');
+let carrinho = [];
+
 
 botao.onmouseup = function(){
-    botao.style.backgroundColor = 'green';
-    botao.innerHTML = 'Item adicionado ao carrinho';
+botao.style.backgroundColor = '#4bdf2e';
+botao.style.border = 'solid green';
+botao.innerHTML = 'Item adicionado a sua lista';
+carrinho[0] = 'arroz';
+localStorage.setItem('lista de compras',carrinho);
 }
 
-
+function armazem (){
+    return JSON.parse(localStorage.getItem(carrinho[0]));
+}
+      
