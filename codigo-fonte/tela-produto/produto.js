ace0b36d074717d256dd9bbeb0da213468093654
davilucas <davilucarios47@gmail.com>
2024-05-22 22:34:13 -0300

    update

--- codigo-fonte/tela-produto/produto.js
+++ codigo-fonte/tela-produto/produto.js
@@ -1,16 +1,24 @@
-let botao = document.querySelector('#botao');
-let carrinho = [];
+var list = [];
+var botao = document.querySelector('#botao');
 
+function addItem(){
+    if(localStorage.listadecompras){
+        list = JSON.parse(localStorage.getItem('listadecompras'));
+        let botao = document.querySelector('#botao');
+        botao.style.backgroundColor = '#4bdf2e';
+        botao.innerHTML = 'Item adicionado a lista de compras';
+    }
 
-botao.onmouseup = function(){
-botao.style.backgroundColor = '#4bdf2e';
-botao.style.border = 'solid green';
-botao.innerHTML = 'Item adicionado a sua lista';
-carrinho[0] = 'arroz';
-localStorage.setItem('lista de compras',carrinho);
+    let novoitem = document.getElementById('v').value;
+    list.push(novoitem);
+    document.getElementById('v').value = "";
+    localStorage.listadecompras = JSON.stringify(list);
+    botao.value = "true";
 }
 
-function armazem (){
-    return JSON.parse(localStorage.getItem(carrinho[0]));
+if(botao === true){
+    botao.style.backgroundColor = '#4bdf2e';
+    botao.innerHTML = 'Item adicionado a lista de compras';
 }
-      
+
+
