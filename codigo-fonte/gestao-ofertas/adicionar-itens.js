a5981f9b55bf0c83a94aff3958a47540147bf101
Trick-Breno <141360060+Trick-Breno@users.noreply.github.com>
2024-06-01 19:51:18 -0300

    atualizacao

--- codigo-fonte/gestao-ofertas/adicionar-itens.js
+++ codigo-fonte/gestao-ofertas/adicionar-itens.js
@@ -1,18 +1,60 @@
+const limiteItensSim = document.getElementById('limiteItensSim');//checkbox
+const inserirQtd = document.querySelector('.qtd-permitida'); //div que contem a campo limite ietns
+const itemPromocao = document.getElementById('itemPromocao');//checkbox
+const validadePromo = document.querySelector('.validadePromo'); // div que contem campo selecionar data
+
+// se o checkbox "limite de itens" for marcado, ira exibir um campo para inserir o limite de itens
+limiteItensSim.addEventListener('change', function() {
+    if (this.checked) {
+        inserirQtd.style.display = 'block';
+    } else {
+        inserirQtd.style.display = 'none';
+    }
+});
+
+// se o checkbox "item em promoçao" for marcado, ira exibir campo escolher data
+itemPromocao.addEventListener('change', function(){
+    if (this.checked) {
+        validadePromo.style.display = 'block';
+    } else {
+        validadePromo.style.display = 'none';
+    }
+});
+
+// formatar a data no formato brasileiro
+function formatarDataBr(dataISO) {
+    const [ano, mes, dia] = dataISO.split('-');
+    return `${dia}/${mes}/${ano}`;
+}
+
+
+// quando formulario for enviado, as informaçoes serao atribuidas a um objeto chamado "item" 
 document.getElementById('itemForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const foto = document.getElementById('foto').value; // Você pode acessar a imagem usando FileReader para armazenar como string base64
+    const foto = document.getElementById('foto').value; 
     const nome = document.getElementById('nome').value;
     const marca = document.getElementById('marca').value;
     const preco = document.getElementById('preco').value;
     const categoria = document.getElementById('categoria').value;
+    const maiorIdadeSim = document.getElementById('maiorIdadeSim').checked;
+    const limiteItensSim = document.getElementById('limiteItensSim').checked;
+    const campoLimiteItem = document.getElementById('campo-limite-item').value;
+    const inicioPromo = document.getElementById('inicioPromo').value;
+    const fimPromo = document.getElementById('fimPromo').value;
+
 
     const item = {
         foto: foto,
         nome: nome,
         marca: marca,
         preco: preco,
-        categoria: categoria
+        categoria: categoria,
+        maiorIdadeSim: maiorIdadeSim,
+        limiteItensSim: limiteItensSim,
+        campoLimiteItem: limiteItensSim ? campoLimiteItem : null,
+        inicioPromo: itemPromocao ? formatarDataBr(inicioPromo) : null,
+        fimPromo: itemPromocao ? formatarDataBr(fimPromo) : null
     };
 
     // Obtendo a lista de itens do localStorage, se não existir, inicialize como um array vazio
@@ -20,6 +62,10 @@ document.getElementById('itemForm').addEventListener('submit', function(event) {
     itens.push(item);
     localStorage.setItem('itens', JSON.stringify(itens));
 
-     // Limpar o formulário
-     document.getElementById('itemForm').reset();
+    // Limpar o formulário
+    document.getElementById('itemForm').reset();
+
+    // Certifique-se de que o campo de limite de itens seja escondido após o reset
+    inserirQtd.style.display = 'none';
+    validadePromo.style.display = 'none';
 });
--- codigo-fonte/gestao-ofertas/meus-itens.js
+++ codigo-fonte/gestao-ofertas/meus-itens.js
@@ -1,15 +1,44 @@
 // Obtendo os itens do localStorage
 const itens = JSON.parse(localStorage.getItem('itens')) || [];
 
+// função para verificar item para maior 18 anos
+function itemMaiorIdade(item) {
+    if (item.maiorIdadeSim) {
+        return '<p>Para maiores de 18 anos</p>';
+    } else {
+        return '';
+    }
+}
+
+function limiteItem(item) {
+    if (item.campoLimiteItem != null) {
+        return `<p> limite de itens por pessoa: ${item.campoLimiteItem}<p>`;
+    } else {
+        return '';
+    }
+}
+
+function dataPromo(item) {
+    if (item.inicioPromo && item.fimPromo != null) {
+        return `<p>Início da promoção: ${item.inicioPromo}</p>
+        <p>Fim da promoção: ${item.fimPromo}</p>`;
+    } else {
+        return '';
+    }
+}
+
 // Exibindo os itens na página
 const itensList = document.getElementById('itensList');
 itens.forEach(item => {
     const itemDiv = document.createElement('div');
     itemDiv.innerHTML = `
         <img src="${item.foto}" alt="Foto do Produto">
-        <p>${item.nome} - ${item.marca}</p>        
+        <p>${item.nome} - ${item.marca}</p>
         <p>Valor R$: ${item.preco}</p>
         <p>Categoria: ${item.categoria}</p>
+        ${itemMaiorIdade(item)}
+        ${limiteItem(item)}
+        ${dataPromo(item)}
         <button onclick="excluirItem(${item.id})">Excluir</button>
     `;
     itensList.appendChild(itemDiv);
@@ -33,3 +62,4 @@ function excluirItem(itemId) {
          location.reload();
     }
 }
+
